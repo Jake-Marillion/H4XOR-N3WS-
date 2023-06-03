ffc697be8da41d5876ba3f5b43ad0dbf788ef80c
Jacob Marillion <jacobmarillion@gmail.com>
2023-06-03 11:18:30 -0600

    Initial Commit

---
+++ H4XOR N3WS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  H4XOR N3WS
+//
+//  Created by Jacob Marillion on 6/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4XOR N3WS/H4XOR_N3WSApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_N3WSApp.swift
+//  H4XOR N3WS
+//
+//  Created by Jacob Marillion on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_N3WSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
